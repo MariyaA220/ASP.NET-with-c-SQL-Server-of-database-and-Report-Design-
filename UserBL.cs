f95e8202210d9cd17ad50d8b822cacc324328fdd
MARIYA AHMED <104404495+MariyaA220@users.noreply.github.com>
2025-03-20 22:50:10 -0700

    Add files via upload
    Student Report Viewing: Displays a detailed report of students, including their degree and branch information.
    Add/Edit/Delete Students: Admin functionality to manage student records.
    Responsive UI: The UI adjusts seamlessly across different devices, thanks to Bootstrap.
    3-Tier Architecture:
    Presentation Layer (UI): The frontend built with HTML, CSS, Bootstrap, and jQuery.
    Business Logic Layer: Handles the processing and validation of data.
    Data Access Layer: Manages database interactions and data retrieval.

---
+++ UserBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+using DataAccess;
+using System.Data;
+
+namespace BusinessLogic
+{
+    public class UserBL
+    {
+        public int storestudentinfo(UserBO objBO1)
+        {
+            UserDA da = new UserDA();
+            return da.addstudentinfo(objBO1);
+        }
+
+       
+        public object Removestudentinfo(UserBO objBO1)
+        {
+            
+            UserDA da = new UserDA();
+            return da.deletestudentinfo(objBO1);
+        }
+      
+    }
+}
---
+++ UserBO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace BusinessObject
+{
+    public class UserBO
+    {
+        private string _StudentName;
+        private DateTime _DateofBirth;
+        private string _Gender;
+        private string _Degree;
+        private string _Branch;
+        private string _EmailId;
+        private string _Mobile;
+
+        public string StudentName
+        {
+            get { return _StudentName; }
+            set { _StudentName = value; }
+        }
+
+        public DateTime DateofBirth
+        {
+            get { return _DateofBirth; }
+            set { _DateofBirth = value; }
+        }
+
+        public string Gender
+        {
+            get { return _Gender; }
+            set { _Gender = value; }
+        }
+
+        public string Degree
+        {
+            get { return _Degree; }
+            set { _Degree = value; }
+        }
+
+        public string Branch
+        {
+            get { return _Branch; }
+            set { _Branch = value; }
+        }
+
+        public string EmailId
+        {
+            get { return _EmailId; }
+            set { _EmailId = value; }
+        }
+
+        public string Mobile
+        {
+            get { return _Mobile; }
+            set { _Mobile = value; }
+        }
+    }
+}
+
